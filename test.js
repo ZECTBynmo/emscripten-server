b7acf04d735e44b9e287a2fc4f332160f38c987e
ZECTBynmo <michael.vegeto@gmail.com>
2013-11-08 11:33:43 -0500

    some test updates

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var app = require("express")(),
 	needle = require('needle');
 
 var sourceCode = {
-	c: "class Tester { public: Tester(){} }"
+	c: "class Tester { public: Tester(){} };"
 };
 
 var url = "http://50.17.82.56:3000/compile";
@@ -11,8 +11,9 @@ console.log( url );
 console.log( sourceCode );
 
 needle.post( url, sourceCode, function(err, res, body) {
-	console.log( body );
-	console.log( body );
-	console.log( body );
-	console.log( body );
+
+	for( var iItem in body ) {
+		console.log( iItem );
+		console.log( body[iItem].toString() );
+	}
 });
