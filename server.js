375f792cabaff93c61dad5c8f9b9168989fe7ec9
ZECTBynmo <michael.vegeto@gmail.com>
2013-11-07 22:02:39 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,37 @@
+var app = require('express')(),
+	fs = require("fs"),
+	ares = require("ares").ares,
+	uuid = require("node-uuid");
+
+var port = 3000;
+
+app.post('/compile', function(req, res) {
+	console.log( req.body );
+
+    var guid = uuid.v1(),
+		filePath = __dirname + "/tmp/" + guid + ".cpp",
+		emscrPath = __dirname + "/../emscripten/emcc";
+
+	fs.writeFile( filePath, req.body.c, function(err) {
+		if( err ) {
+			res.json( 500, {"error": "Failed to write out cpp file: " + err} );
+		} else {
+			var outputPath = __dirname + "/tmp/" + guid + ".js",
+				command = emscrPath + " " + filePath + " -o " + outputPath;
+
+			ares( command, true, function() {
+				fs.readFile( outputPath, function(error, data) {
+					if( error ) {
+						res.json( 500, {"error": "Failed to compile source file: " + error} );
+					} else {
+						res.json( 200, {"js": data} );
+					}
+				});
+			});
+		}
+	});
+});
+
+
+app.listen( port );
+console.log( "Listening on port " + port );
---
+++ test.js
@@ -0,0 +1,16 @@
+var app = require("express")(),
+	needle = require('needle');
+
+var sourceCode = {"c":"class Tester { public: Tester(){} }"};
+
+var url = "http://50.17.82.56:3000/compile";
+
+console.log( url );
+console.log( sourceCode );
+
+needle.post( url, sourceCode, function(err, res, body) {
+	console.log( body );
+	console.log( body );
+	console.log( body );
+	console.log( body );
+});
