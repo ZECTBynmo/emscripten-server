80f308a8ed8210be0890fd4fc8b48aeeee713a3a
ZECTBynmo <michael.vegeto@gmail.com>
2013-11-08 22:49:13 -0500

    update test url and output name

--- test.js
+++ test.js
@@ -5,12 +5,12 @@ var sourceCode = {
 	c: "class Tester { public: Tester(){} };"
 };
 
-var url = "http://50.17.82.56:3000/compile";
+var url = "http://54.243.208.160:3000/compile";
 
 console.log( url );
 console.log( sourceCode );
 
 needle.post( url, sourceCode, function(err, res, body) {
 
-	require("fs").writeFileSync( __dirname + "/test.js", body.js );
+	require("fs").writeFileSync( __dirname + "/testoutput.js", body.js );
 });
